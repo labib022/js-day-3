10edf8581ecb3e74d832408f15817bdf69ee1450
Labib Hasan <labibshawon101@gmail.com>
2025-08-02 23:53:00 +0600

    Day 6 update

--- day-4.js
+++ day-4.js
@@ -1,109 +1,109 @@
-// let fruits = ["Apple", "Banana", "Mango","Orange","Lichi"];
-// console.log(fruits.length);
-// console.log(fruits[3]);
+// // let fruits = ["Apple", "Banana", "Mango","Orange","Lichi"];
+// // console.log(fruits.length);
+// // console.log(fruits[3]);
 
-// for (let i=0; i< fruits.length; i++) {
-//     console.log(fruits[i]);
-// }
+// // for (let i=0; i< fruits.length; i++) {
+// //     console.log(fruits[i]);
+// // }
 
-// for (let fruit of fruits) {
-//     console.log(fruit);
-// }
+// // for (let fruit of fruits) {
+// //     console.log(fruit);
+// // }
 
-// let numbers = [1,2,3,4,5,6,7,8,9,10];
+// // let numbers = [1,2,3,4,5,6,7,8,9,10];
 
-//  let squres =  numbers.map(n => n * n);
-//  console.log(squres);
+// //  let squres =  numbers.map(n => n * n);
+// //  console.log(squres);
 
-//  let even = numbers.filter(n => n % 2 === 0);
-//  console.log(even);
+// //  let even = numbers.filter(n => n % 2 === 0);
+// //  console.log(even);
 
-//    numbers.shift();
-//    console.log(numbers);
+// //    numbers.shift();
+// //    console.log(numbers);
 
-//  let found = numbers.find(num => num > 3);
-//  console.log(found);
+// //  let found = numbers.find(num => num > 3);
+// //  console.log(found);
 
-//  let person = {
-//      name: "Labib", 
-//     age: 22,
-//     city: "Dhaka"
+// //  let person = {
+// //      name: "Labib", 
+// //     age: 22,
+// //     city: "Dhaka"
 
-//  };
-//  console.log(person);
+// //  };
+// //  console.log(person);
 
-//  let student = {
-//      name: "Rafi",
-//      subjects: ["Math", "English", "Physics"]
-//  };
-//  console.log(student.subjects[2]);
-
-
+// //  let student = {
+// //      name: "Rafi",
+// //      subjects: ["Math", "English", "Physics"]
+// //  };
+// //  console.log(student.subjects[2]);
 
 
 
 
 
-//// Practice Exercise---------///
 
 
-// let names = ["Messi", "De Maria", "Emi", "Alvarez","Enzo"];
-// names.map(name => console.log(name));
+// //// Practice Exercise---------///
 
-// let numbers = [1,2,3,4,5,6,7,8,9,10];
-// let even = numbers.filter(n => n % 2 === 0);
-// console.log(even);
+
+// // let names = ["Messi", "De Maria", "Emi", "Alvarez","Enzo"];
+// // names.map(name => console.log(name));
+
+// // let numbers = [1,2,3,4,5,6,7,8,9,10];
+// // let even = numbers.filter(n => n % 2 === 0);
+// // console.log(even);
  
-// let person = {
-//   name: "Labib",
-//   age: 22,
-//   city: "Dhaka"
-// };
+// // let person = {
+// //   name: "Labib",
+// //   age: 22,
+// //   city: "Dhaka"
+// // };
 
-// for (let key in person){
-//     console.log(key + ": " + person[key]);
-// }
+// // for (let key in person){
+// //     console.log(key + ": " + person[key]);
+// // }
 
 
-//   let subjects = {
-//     name: "Panda",
-//     age : 15,
-//     subjects : ["Math", "Physics", "Chemistry","English","biology"]
-//   };
+// //   let subjects = {
+// //     name: "Panda",
+// //     age : 15,
+// //     subjects : ["Math", "Physics", "Chemistry","English","biology"]
+// //   };
   
 
-//   function sum(...nums) {
+// //   function sum(...nums) {
+// //     return nums.reduce((acc, val) => acc + val, 0);
+// //   }
+// //   console.log(sum(1,2,5));
+
+//   const numbered = [50, 100, 150];
+//   const [first, second] = numbered;
+//   console.log(first);
+//   console.log(second);
+
+
+// const person = {
+//     name: "Shawon",
+//     age : 25,
+//     city:"Dhaka",
+// }
+
+// const {name, age} = person;
+// console.log(name);
+// console.log(age);
+
+// const user = {name: "Labib", age:26};
+// const updatedUser = {...user, city: "Dhaka"};
+
+// console.log(updatedUser);
+
+
+// function sum(...nums) {
 //     return nums.reduce((acc, val) => acc + val, 0);
-//   }
-//   console.log(sum(1,2,5));
+// }
+// console.log(sum(5,10,15));
 
-  const numbered = [50, 100, 150];
-  const [first, second] = numbered;
-  console.log(first);
-  console.log(second);
-
-
-const person = {
-    name: "Shawon",
-    age : 25,
-    city:"Dhaka",
-}
-
-const {name, age} = person;
-console.log(name);
-console.log(age);
-
-const user = {name: "Labib", age:26};
-const updatedUser = {...user, city: "Dhaka"};
-
-console.log(updatedUser);
-
-
-function sum(...nums) {
-    return nums.reduce((acc, val) => acc + val, 0);
-}
-console.log(sum(5,10,15));
-
-let age = 24;
-let result = age >= 18 ? "Adult" : "Minor";
-console.log(result);
+// let age = 24;
+// let result = age >= 18 ? "Adult" : "Minor";
+// console.log(result);
